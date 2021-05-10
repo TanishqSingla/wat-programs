9f5bea39b97bef5095e4e1d38b73360a7bc8bc45
TanishqSingla <tanishqsingla00@gmail.com>
2021-05-10 15:04:52 +0530

    adding an export object which will be imported in wasm module

--- 2-Hello-world/helloworld.js
+++ 2-Hello-world/helloworld.js
@@ -2,6 +2,19 @@ const fs = require("fs");
 
 const bytes = fs.readFileSync(__dirname + "/hello-world.wasm");
 
-let hello_world = null;
-let start_string_index = 100;
+let helloWorld = null;
+let startStringIndex = 100;
 let memory = new WebAssembly.Memory({ initial: 1 });
+
+let importObject = {
+  env: {
+    buffer: memory,
+    start_string: startStringIndex,
+    print_string: function (str_len) {
+      const bytes = new Uint8Array(memory.buffer, startStringIndex, str_len);
+
+      const log_string = new TextDecoder("utf8").decode(bytes);
+      console.log(log_string);
+    },
+  },
+};
