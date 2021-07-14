6eff8259f1319c7089648477b141295c04b0d3e9
TanishqSingla <tanishqsingla00@gmail.com>
2021-07-14 17:26:14 +0530

    Added optimied version of wasm file

--- 17-Canvas/collide.js
+++ 17-Canvas/collide.js
@@ -19,49 +19,56 @@ const memory = new WebAssembly.Memory({ initial: 80 });
 const mem_i8 = new Uint8Array(memory.buffer);
 const mem_i32 = new Uint32Array(memory.buffer);
 const importObject = {
-    env: {
-        buffer: memory,
-        cnvs_size,
-        no_hit_color,
-        hit_color,
-        obj_start,
-        obj_cnt,
-        obj_size,
-        x_offset,
-        y_offset,
-        xv_offset,
-        yv_offset,
-    },
+  env: {
+    buffer: memory,
+    cnvs_size,
+    no_hit_color,
+    hit_color,
+    obj_start,
+    obj_cnt,
+    obj_size,
+    x_offset,
+    y_offset,
+    xv_offset,
+    yv_offset,
+  },
 };
-const image_data = new ImageData(new Uint8ClampedArray(memory.buffer, 0, obj_start), cnvs_size, cnvs_size);
+const image_data = new ImageData(
+  new Uint8ClampedArray(memory.buffer, 0, obj_start),
+  cnvs_size,
+  cnvs_size
+);
 const stride_i32 = stride_bytes / 4;
 for (let i = 0; i < obj_cnt * stride_i32; i += stride_i32) {
-    // value less than canvas_size
-    let temp = Math.floor(Math.random() * cnvs_size);
-    // set object x attribute to random value
-    mem_i32[obj_start_32 + i] = temp;
-    // random value less than canvas_size
-    temp = Math.floor(Math.random() * cnvs_size);
-    // set object y attribute to random value
-    mem_i32[obj_start_32 + i + 1] = temp;
-    // random value between -2 and 2
-    temp = Math.round(Math.random() * 4) - 2;
-    // set x velocity to random value
-    mem_i32[obj_start_32 + i + 2] = temp;
-    // random value between -2 and 2
-    temp = Math.round(Math.random() * 4) - 2;
-    // set y velocity to random value
-    mem_i32[obj_start_32 + i + 3] = temp;
+  // value less than canvas_size
+  let temp = Math.floor(Math.random() * cnvs_size);
+  // set object x attribute to random value
+  mem_i32[obj_start_32 + i] = temp;
+  // random value less than canvas_size
+  temp = Math.floor(Math.random() * cnvs_size);
+  // set object y attribute to random value
+  mem_i32[obj_start_32 + i + 1] = temp;
+  // random value between -2 and 2
+  temp = Math.round(Math.random() * 4) - 2;
+  // set x velocity to random value
+  mem_i32[obj_start_32 + i + 2] = temp;
+  // random value between -2 and 2
+  temp = Math.round(Math.random() * 4) - 2;
+  // set y velocity to random value
+  mem_i32[obj_start_32 + i + 3] = temp;
 }
 var animation_wasm;
 function animate() {
-    animation_wasm();
-    ctx.putImageData(image_data, 0, 0);
-    requestAnimationFrame(animate);
+  animation_wasm();
+  ctx.putImageData(image_data, 0, 0);
+  requestAnimationFrame(animate);
 }
 (async () => {
-    let obj = await WebAssembly.instantiateStreaming(fetch("collide.wasm"), importObject);
-    // @ts-expect-error
-    animation_wasm = obj.instance.exports.main;
-    requestAnimationFrame(animate);
+  let obj = await WebAssembly.instantiateStreaming(
+    fetch("collide-3.wasm"),
+    importObject
+  );
+  // @ts-expect-error
+  animation_wasm = obj.instance.exports.main;
+  requestAnimationFrame(animate);
 })();
